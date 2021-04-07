bce1172ce225af3f80f770fcedb03ac4a515956f
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-04-06 21:28:07 -0400

    Initial files

---
+++ src/main/java/rosenfeld/paint/PaintCanvas.java
@@ -0,0 +1,29 @@
+
+package rosenfeld.paint;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class PaintCanvas extends Canvas {
+
+//    public void draw(Rocket rocket) {
+//        GraphicsContext context = getGraphicsContext2D();
+//
+//        context.setFill(Color.VIOLET);
+//        context.fillRect(0,0, getWidth(), getHeight());
+//        context.translate(0, getHeight());
+//
+//        context.setFill(Color.BLUE);
+//        for (double time=0; time<rocket.getFlightTime(); time+=0.5) {
+//            double x = rocket.getX(time);
+//            double y = rocket.getY(time);
+//            context.fillOval(x, -y, 10, 10);
+//            context.lineTo(x, -y);
+//            context.stroke();
+//        }
+//
+//        context.translate(0, -getHeight());
+//    }
+
+}
