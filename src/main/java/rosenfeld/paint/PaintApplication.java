7df23c7d4f91175e521b181f76fe871a9bd5a730
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-04-06 21:49:43 -0400

    Playing with color picker

--- src/main/java/rosenfeld/paint/PaintApplication.java
+++ src/main/java/rosenfeld/paint/PaintApplication.java
@@ -1,9 +1,17 @@
 package rosenfeld.paint;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PaintApplication extends Application {
@@ -11,11 +19,34 @@ public class PaintApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/paint_application.fxml"));
-
-        Scene scene = new Scene(root, 300, 300);
+//        Parent root = FXMLLoader.load(getClass().getResource("/paint_application.fxml"));
+//
+//        Scene scene = new Scene(root, 300, 300);
+//
+//        stage.setTitle("Paint");
+//        stage.setScene(scene);
+//        stage.show();
 
         stage.setTitle("Paint");
+        Scene scene = new Scene(new HBox(20), 300, 300);
+        HBox box = (HBox) scene.getRoot();
+        box.setPadding(new Insets(5, 5, 5, 5));
+
+        final ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.CORAL);
+
+        final Text text = new Text("Current Color");
+        text.setFont(Font.font ("Verdana", 10));
+        text.setFill(colorPicker.getValue());
+
+        colorPicker.setOnAction(new EventHandler() {
+            public void handle(Event t) {
+                text.setFill(colorPicker.getValue());
+            }
+        });
+
+        box.getChildren().addAll(colorPicker, text);
+
         stage.setScene(scene);
         stage.show();
 
